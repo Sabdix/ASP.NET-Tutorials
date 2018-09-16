b5caf82f59d03a975ff2024616c82bd325eec34d
Sabdi Abraham Pantoja Orozco <sabdi_10@hotmail.com>
2018-09-16 18:34:38 -0500

    MVC Introduction

---
+++ mvc Tutorial 2/mvc Tutorial 2/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using mvc_Tutorial_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_Tutorial_2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shreck!" };
+            return View(movie);
+        }
+    }
+}
