bed1151a454a9c3d64e9090f0f5d1ee57a9c2b84
Sabdi Abraham Pantoja Orozco <sabdi_10@hotmail.com>
2018-09-16 16:41:04 -0500

    mvc

---
+++ mvc tutorial/mvc tutorial/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using mvc_tutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_tutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var persona = new Persona();
+            return View(persona);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Persona persona)
+        {
+            return View(persona);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ mvc tutorial/mvc tutorial/Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_tutorial.Models
+{
+    public class Persona
+    {
+        public string Saludo { get; set; }
+        public string Nombre { get; set; }
+
+        public Persona()
+        {
+            Saludo = "Hola";
+            Nombre = string.Empty;
+        }
+    }
+}
