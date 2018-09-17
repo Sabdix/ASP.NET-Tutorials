a0727e5530c3228f2f490b5b9e88f1ac1f0c6726
Sabdi Abraham Pantoja Orozco <sabdi_10@hotmail.com>
2018-09-16 19:52:37 -0500

    Custome Routes

--- mvc Tutorial 2/mvc Tutorial 2/Controllers/MoviesController.cs
+++ mvc Tutorial 2/mvc Tutorial 2/Controllers/MoviesController.cs
@@ -13,7 +13,30 @@ namespace mvc_Tutorial_2.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shreck!" };
-            return View(movie);
+            //return new EmptyResult();
+            //return HttpNotFound();
+            //return Content("Hello World");
+            //return View(movie);
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
